5e2de25f5d81fa964e025a54ba4dacc97ae65055
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-07-29 20:29:43 +0600

    6 kyu detect pangram

--- README.md
+++ README.md
@@ -10,6 +10,7 @@
 
 | Kyu | Name                                                                                                     | Kata Link                                                    |
 | --- | -------------------------------------------------------------------------------------------------------- | ------------------------------------------------------------ |
+| 6   | [Detect pangram](javascript/6_kyu_detect_pangram.js)                                                     | [→](https://www.codewars.com/kata/545cedaa9943f7fe7b000048)  |
 | 7   | [Sum of two lowest positive integers](javascript/7_kyu_sum_of_two_lowest_positive_integer.js)            | [→](https://www.codewars.com/kata/558fc85d8fd1938afb000014)  |
 | 7   | [Find the next perfect square](javascript/7_kyu_find_the_next_perfect_square.js)                         | [→](https://www.codewars.com/kata/56269eb78ad2e4ced1000013)  |
 | 8   | [DNA to RNA Conversion](javascript/8_kyu_dna_to_rna_conversion.js)                                       | [→](https://www.codewars.com/kata/5556282156230d0e5e000089/) |
---
+++ javascript/6_kyu_detect_pangram.js
@@ -0,0 +1,42 @@
+/**
+ * Kata link: https://www.codewars.com/kata/545cedaa9943f7fe7b000048
+ *
+ * A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+ *
+ * Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+ *
+ *--------------------------------------
+ *  Expected outputs:
+ *
+ * console.log(isPangram('The quick brown fox jumps over the lazy dog.'), true)
+ * console.log(isPangram('This is not a pangram.'), false)
+ * */
+
+/**
+ *
+ * @param {string} string
+ * @returns {boolean}
+ */
+
+function isPangram(string) {
+  return (
+    string
+      .toLowerCase()
+      .replace(/[^a-z]/gi, '')
+      .split('')
+      .sort()
+      .join('')
+      .replace(/(\w)(\1+)/g, '$1').length === 26
+  )
+}
+
+// -------------------------------------
+// Other solutions
+//---------------------------------------
+
+// https://www.codewars.com/kata/reviews/54a5d4af37f43531e900002e/groups/5745b6ccfd21f034a80016e0
+// 13% faster than my solution based on jsbench
+
+function isPangram(string) {
+  return (string.match(/([a-z])(?!.*\1)/gi) || []).length === 26
+}
